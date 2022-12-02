fe65f40ca447b3e9ebcd08c8fb119598daf6f99d
Fernando Moyano <nanomoyano@gmail.com>
2022-12-02 11:07:27 +0100

    More renaming co2 to nee

--- mod_ra1.py
+++ mod_ra1.py
@@ -1,7 +1,7 @@
 # mod_ra1.py 
 # Python module file: Recovery Analysis 1
 #
-# Functions to analyse CO2 fluxes and other land surface variables
+# Functions to analyse C fluxes and other land surface variables
 
 # Imports
 import rioxarray as rio
@@ -10,45 +10,45 @@ import numpy as np
 from xarrayutils.utils import linear_trend
 
 
-def proc_neeInv(invco2_file, roi_file = None, nodata_val = None):
+def proc_neeInv(neeInv_file, roi_file = None, nodata_val = None):
 
     # All three file reading methods read in the attributes differently.
-    # co2InvRaw = rio.open_rasterio(file_path)
-    # co2InvRaw = xr.open_dataset(file_path, engine="rasterio", variable='co2flux_land')
-    co2InvRaw_List = rio.open_rasterio(invco2_file, variable='co2flux_land')
-    co2InvRaw = co2InvRaw_List['co2flux_land']  # Extracting the dataarray from the list
-    co2InvRaw = co2InvRaw.rename({'mtime': 'time'})  # Rename the time dimension and coordinate to something standard.
-    co2InvRaw.attrs['units'] = 'PgC/yr'  # For some reason the units is a tuple of repeating values. Replacing with single value.
-    co2InvRaw.rio.write_crs(4326, inplace=True)  # Set the crs: in this case the data is in epsg4326. This creates the spatial_ref coordinate.
+    # neeInvRaw = rio.open_rasterio(file_path)
+    # neeInvRaw = xr.open_dataset(file_path, engine="rasterio", variable='co2flux_land')
+    neeInvRaw_List = rio.open_rasterio(neeInv_file, variable='co2flux_land')
+    neeInvRaw = neeInvRaw_List['co2flux_land']  # Extracting the dataarray from the list
+    neeInvRaw = neeInvRaw.rename({'mtime': 'time'})  # Rename the time dimension and coordinate to something standard.
+    neeInvRaw.attrs['units'] = 'PgC/yr'  # For some reason the units is a tuple of repeating values. Replacing with single value.
+    neeInvRaw.rio.write_crs(4326, inplace=True)  # Set the crs: in this case the data is in epsg4326. This creates the spatial_ref coordinate.
 
     # Setting nodata
     if(nodata_val is not None):
-        co2InvRaw = co2InvRaw.rio.write_nodata(nodata_val)  # Define what value represents nodata
-        co2InvRaw = co2InvRaw.rio.write_nodata(co2InvRaw.rio.nodata, encoded=True, inplace=True)  # Set as encoded. This changes the values to NaN
+        neeInvRaw = neeInvRaw.rio.write_nodata(nodata_val)  # Define what value represents nodata
+        neeInvRaw = neeInvRaw.rio.write_nodata(neeInvRaw.rio.nodata, encoded=True, inplace=True)  # Set as encoded. This changes the values to NaN
     
     # Get the pixel area and change the units to tC/ha/y
-    pixarea_List = rio.open_rasterio(invco2_file, variable='area')
+    pixarea_List = rio.open_rasterio(neeInv_file, variable='area')
     pixarea = pixarea_List['area']
     landarea = pixarea[0]
     landarea.attrs['units'] = 'm2'
     landarea.coords.__delitem__('rt')
 
-    # Convert the co2 flux units: pixel to sqm, then to ha; and petagram to tons, so PgC y-1 -> tC ha-1 y-1
-    co2Inv = co2InvRaw / landarea * 10000 * 10**9 # to ha, to tons.
+    # Convert the NEE units: pixel to sqm, then to ha; and petagram to tons, so PgC y-1 -> tC ha-1 y-1
+    neeInv = neeInvRaw / landarea * 10000 * 10**9 # to ha, to tons.
 
     # Add some important attributes
-    co2Inv.attrs['units'] = 'tC/ha/y'
-    co2Inv.attrs['long_name'] = 'Land-Atmosphere CO2 flux'
+    neeInv.attrs['units'] = 'tC/ha/y'
+    neeInv.attrs['long_name'] = 'Net Ecosystem Exchange'
 
     if(roi_file is not None):
         roi = gpd.read_file(roi_file)
-        co2InvS = co2Inv.rio.clip(roi.geometry, roi.crs, all_touched=True)
+        neeInvS = neeInv.rio.clip(roi.geometry, roi.crs, all_touched=True)
 
-    return(co2InvS)
+    return(neeInvS)
 
 def get_neeSeasAmp(nee):
     """
-    This function calculates the yearly aplitude between winter and summer months co2 fluxes.
+    This function calculates the yearly aplitude between winter and summer months C fluxes.
     nee must be an xarray array with monthly data.
     
     # Calculating seasonal amplitudes and temporal amplitude trends
@@ -69,8 +69,8 @@ def get_neeSeasAmp(nee):
     nee['groups'] = ('time', groups)
 
     # Calculate the mean for each year-season group
-    # co2InvTG = invnee.groupby(['time.year', 'season']).mean()  # Does not work! Grouping by two variables is not supported in xarray!
-    neeSeasRate = nee.groupby('groups').mean()  # Get the mean values of surface co2 fluxes by year-season
+    # neeInvTG = invnee.groupby(['time.year', 'season']).mean()  # Does not work! Grouping by two variables is not supported in xarray!
+    neeSeasRate = nee.groupby('groups').mean()  # Get the mean values of surface C fluxes by year-season
     # Calculate the total seasonal flux by multiplying the mean by the 6 month time period (i.e. 0.5 years)
     neeSeas = neeSeasRate * 0.5
     # Add again the coordinate representing years
@@ -78,10 +78,10 @@ def get_neeSeasAmp(nee):
     neeSeas['year'] = ('groups', years)
 
     # Check results:
-    # print(co2InvTG[0:,0,0])
-    # print(co2InvTG.coords['groups'])
-    # print(co2InvTG.coords['year'])
-    # co2InvTG.where(co2InvTG.year==1985, drop=True)
+    # print(neeInvTG[0:,0,0])
+    # print(neeInvTG.coords['groups'])
+    # print(neeInvTG.coords['year'])
+    # neeInvTG.where(neeInvTG.year==1985, drop=True)
 
     # Calculate the difference between winter (positive) and summer (negative) mean fluxes. For the southern hemisphere the sign is corrected afterwards.
     def diff(x):
@@ -92,7 +92,7 @@ def get_neeSeasAmp(nee):
     # Rename the xarray data array and setting the crs
     neeSeasDiff = neeSeasDiff.rename('nee_yearlyamp')
     neeSeasDiff.rio.write_crs(4326, inplace=True)
-    neeSeasDiff.attrs['long_name'] = 'CO2 flux seasonal amplitude'
+    neeSeasDiff.attrs['long_name'] = 'NEE seasonal amplitude'
     neeSeasDiff.attrs['units'] = 'tC/ha/y'
 
     return(neeSeasDiff)
@@ -106,20 +106,20 @@ def get_neeAmpStats(neeAmp, period=None):
 
     fluxamp_stats = linear_trend(neeAmp, 'year')
     fluxamp_stats.slope.attrs['units'] = 'tC/ha/y'
-    fluxamp_stats.slope.attrs['long_name'] = 'Trend in seasonal CO2 flux amplitude'
+    fluxamp_stats.slope.attrs['long_name'] = 'Trend in seasonal NEE amplitude'
     fluxamp_stats.rio.write_crs(4326, inplace=True)
 
     # Variance
     fluxamp_var = neeAmp.var(dim='year')
     fluxamp_var.attrs['units'] = 'tC/ha/y'
-    fluxamp_var.attrs['long_name'] = 'Temporal variance in seasonal CO2 flux amplitude'
+    fluxamp_var.attrs['long_name'] = 'Temporal variance in seasonal NEE amplitude'
     fluxamp_var.rio.write_crs(4326, inplace=True)
     fluxamp_var = fluxamp_var.rename('neeamp_variance')
 
     # Mean
     fluxamp_mean = neeAmp.mean(dim='year')
     fluxamp_mean.attrs['units'] = 'tC/ha/y'
-    fluxamp_mean.attrs['long_name'] = 'Temporal mean in seasonal CO2 flux amplitude'
+    fluxamp_mean.attrs['long_name'] = 'Temporal mean in seasonal NEE amplitude'
     fluxamp_mean.rio.write_crs(4326, inplace=True)
     fluxamp_mean = fluxamp_mean.rename('neeamp_mean')
 
